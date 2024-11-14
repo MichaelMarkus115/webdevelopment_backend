86969eae4bff7a0a50d44804bc20cd3259667b7c
Michael van den Berg <michaelmarkus115@gmail.com>
2024-11-14 09:38:08 +0200

    commit

--- dao/moviesDAO.js
+++ dao/moviesDAO.js
@@ -1,4 +1,8 @@
+import mongodb from "mongodb";
+const ObjectId = mongodb.ObjectID;
+
 let movies;
+
 export default class MoviesDAO {
   static async injectDB(conn) {
     if (movies) {
@@ -8,6 +12,34 @@ export default class MoviesDAO {
       movies = await conn.db(process.env.MOVIEREVIEWS_NS).collection("movies");
     } catch (e) {
       console.error(`unable to connect in MoviesDAO: ${e}`);
+    }
+  }
+
+  static async getMovieById(id) {
+    try {
+      //use aggregate to provide a sequence of data aggregation operations
+      return await movies
+        .aggregate([
+          {
+            //look for movie doc that matches specified id
+            $match: {
+              _id: new ObjectId(id),
+            },
+          },
+          // joins id field from movie doc to movie_id in reviews collection
+          {
+            $lookup: {
+              from: "reviews",
+              localField: "_id",
+              foreignField: "movie_id",
+              as: "reviews",
+            },
+          },
+        ])
+        .next();
+    } catch (e) {
+      console.error(`something went wrong in getMovieById: ${e}`);
+      throw e;
     }
   }
 
@@ -25,6 +57,7 @@ export default class MoviesDAO {
         query = { rated: { $eq: filters["rated"] } };
       }
     }
+
     let cursor;
     try {
       cursor = await movies
@@ -37,6 +70,19 @@ export default class MoviesDAO {
     } catch (e) {
       console.error(`Unable to issue find command, ${e}`);
       return { moviesList: [], totalNumMovies: 0 };
+    }
+  }
+
+  static async getRatings() {
+    let ratings = [];
+    try {
+      //look for all possible ratings for all movies in movie object
+      ratings = await movies.distinct("rated");
+      return ratings;
+    } catch (e) {
+      //error message
+      console.error(`unable to get ratings, ${e}`);
+      return ratings;
     }
   }
 }
--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 import app from "./server.js";
 import mongodb from "mongodb";
 import dotenv from "dotenv";
-import MoviesDAO from './dao/moviesDAO.js'
+import MoviesDAO from "./dao/moviesDAO.js";
+import ReviewsDAO from "./dao/reviewsDAO.js";
+
 async function main() {
   dotenv.config();
-  const client = new mongodb.MongoClient(process.env.MOVIEREVIEWS_DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+  const client = new mongodb.MongoClient(process.env.MOVIEREVIEWS_DB_URI);
   const port = process.env.PORT || 8000;
+
   try {
     // Connect to the MongoDB cluster
     await client.connect();
-    await MoviesDAO.injectDB(client)
+    await MoviesDAO.injectDB(client);
+    await ReviewsDAO.injectDB(client);
+
     app.listen(port, () => {
       console.log("server is running on port:" + port);
     });
@@ -18,4 +23,5 @@ async function main() {
     process.exit(1);
   }
 }
+
 main().catch(console.error);
